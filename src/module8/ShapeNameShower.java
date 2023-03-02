42f30dc3e21da01e753e3ae9c9276d95a702e05a
Eugene <lugovoj20@gmail.com>
2023-03-02 12:24:26 +0200

    Change ShapeNameShower & ShapeTest

--- src/module8/ShapeNameShower.java
+++ src/module8/ShapeNameShower.java
@@ -2,6 +2,7 @@ package module8;
 
 public class ShapeNameShower {
     public static void nameShower(Shape shape){
+        if (shape == null) return;
         System.out.println(shape.getName());
     }
 }
--- src/module8/ShapesTest.java
+++ src/module8/ShapesTest.java
@@ -6,12 +6,13 @@ public class ShapesTest {
         Circle circle = new Circle(8);
         Rectangle rectangle = new Rectangle(4,2);
         Ellipse ellipse = new Ellipse(7,8);
+        ellipse = null;
         Quad quad = new Quad(6);
 
         Shape [] shapes = {triangle, circle, rectangle, ellipse, quad};
 
         for (Shape sh : shapes){
-            System.out.println(sh.getName());
+            ShapeNameShower.nameShower(sh);
         }
     }
 }
