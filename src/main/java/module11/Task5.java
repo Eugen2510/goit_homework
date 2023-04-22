4e29a7823cabe08329b89a63c87ac23389316d7d
Eugene Luhovyi <logovoj20@gmail.com>
2023-04-22 21:25:42 +0300

    Add task 5

--- src/main/java/module11/Task5.java
+++ src/main/java/module11/Task5.java
@@ -1,4 +1,23 @@
 package module11;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Task5 {
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
+        Iterator<T> iteratorFirst = first.iterator();
+        Iterator<T> iteratorSecond = second.iterator();
+
+        List<T> res = new ArrayList<>();
+
+        while (iteratorFirst.hasNext() && iteratorSecond.hasNext()){
+            res.add(iteratorFirst.next());
+            res.add(iteratorSecond.next());
+        }
+
+        return res.stream();
+
+    }
 }
